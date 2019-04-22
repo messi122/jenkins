32a97d2ca45f6766a7e482529445e4b80dda4c16
yuyong <1103023317@qq.com>
2019-04-22 11:16:20 +0800

    king

---
+++ src/main/java/com/example/jenkins/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.example.jenkins.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @ClassName: com.example.jenkins.controller
+ * @Author: yuyong
+ * @CreateDate: 2019/4/22 11:15
+ * @UpdateUser:
+ * @UpdateDate: 2019/4/22 11:15
+ * @UpdateRemark:
+ * @Version: 1.0
+ * @since JDK 1.8
+ */
+@RestController
+@RequestMapping(value = "/index")
+public class IndexController {
+
+    public String index(){
+        return "hello messi !!!!";
+    }
+}
